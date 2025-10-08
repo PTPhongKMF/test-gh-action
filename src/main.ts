f5aea7fa002f3310b4206738c1e96bb92b7e443b
PTPhongKMF <169053895+PTPhongKMF@users.noreply.github.com>
2025-10-09 03:05:50 +0700

    3

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import core from "@actions/core";
+import process from "node:process";
 
 function main() {
   try {
     const token = core.getInput("token");
     const name = core.getInput("name", { required: true });
 
-    core.info("token: " + token);
+    core.info("token: " + (token ?? process.env.GITHUB_TOKEN));
   } catch (error) {
     core.setFailed("❌ An unexpected error occurred:\n" + error);
   }
