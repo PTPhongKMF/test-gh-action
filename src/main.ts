5f001ec4a693fb881d2af3e7df28993a25c0bb86
PTPhongKMF <169053895+PTPhongKMF@users.noreply.github.com>
2025-10-09 20:53:20 +0700

    test 5

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function main() {
     core.info(`👤 Using name: ${name}`);
     core.info(`🏷️ Repo: ${owner}/${repo}`);
 
-    // Create a test branch name dsdsewewesasasaswewewewewesdsdsdsdsdsdsdsdsd
+    // Create a test branch name dsdsewewesasasaswewewewewesdsdsdsdsdsdsdsdsddsdsdsdsd
     const branchName = `test/pr-${Date.now()}`;
 
     // Get the latest commit SHA from main
